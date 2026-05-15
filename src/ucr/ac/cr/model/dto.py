df17c3b1e3cd428c762260a4b6379e1ab476eb01
aracellyarayagutierrez <aracellyarayagutierrez0@gmail.com>
2026-05-15 16:18:53 -0600

    Aracelly Araya
    Se arreglo un error de atributos en el dto, para que la ventana de paticipaciones se ejecutara bien

--- src/ucr/ac/cr/model/dto.py
+++ src/ucr/ac/cr/model/dto.py
@@ -1,7 +1,7 @@
 class ParticipacionViewDTO:
 
-    def __init__(self, id, voluntario_nombre, actividad, horas):
+    def __init__(self, id, voluntario_nombre, actividad_nombre, horas):
         self.id = id
         self.voluntario_nombre = voluntario_nombre
-        self.actividad = actividad
+        self.actividad_nombre = actividad_nombre
         self.horas = horas
